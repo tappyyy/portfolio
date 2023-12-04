acb614bd02317e57ab77697e095785e2fcfc1c02
hamzziii <steffitann26@gmail.com>
2023-12-05 00:22:57 +0700

    updated

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -61,11 +62,50 @@ if (currentTheme) {
 
 //Adding date
 
-let myDate = document.querySelector("#datee");
+// let myDate = document.querySelector("#datee");
 
-const yes = new Date().getFullYear();
-myDate.innerHTML = yes;
+// const yes = new Date().getFullYear();
+// myDate.innerHTML = yes;
 
+/**
+ * element toggle function
+ */
 
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
+/**
+ * header sticky & go to top
+ */
 
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) {
+      elemToggleFunc(toggleBtns[i]);
+    }
+    elemToggleFunc(skillsBox);
+  });
+}
